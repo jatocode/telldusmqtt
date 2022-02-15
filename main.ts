062404d551ca29003c738ad685773b11e649dd26
Tobias Jansson <Tobias.Jansson@prevas.se>
2022-02-15 10:50:47 +0100

    First try with deno mqtt

---
+++ main.ts
@@ -0,0 +1,33 @@
+import { Client } from 'https://deno.land/x/mqtt/deno/mod.ts'; // Deno (ESM)
+// const { Client } = require('@jdiamond/mqtt'); // Node.js (CommonJS)
+// import { Client } from 'https://unpkg.com/@jdiamond/mqtt-browser'; // Browsers (ESM)
+
+async function main() {
+    const client = new Client({
+        url: 'mqtt://mqtt.taklamakan.se',
+    });
+
+    await client.connect();
+
+    const decoder = new TextDecoder();
+
+    client.on('message', async (topic: string, payload: Uint8Array) => {
+
+        const jsondata = decoder.decode(payload);
+
+        switch (topic) {
+            case 'tellstick':
+                const data = JSON.parse(jsondata)
+                console.log(data)
+                const p=Deno.run({ cmd: ["echo", data.cmd, data.id] });
+                await p.status();
+                break
+            default:
+                break
+        }
+    });
+
+    await client.subscribe('#');
+}
+
+main();
