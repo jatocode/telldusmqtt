2ab92ac380eb24f93839149f4cea596e18b9d258
Tobias Jansson <Tobias.Jansson@prevas.se>
2022-02-15 12:09:51 +0100

    Adding it all together

--- dockerfile
+++ dockerfile
@@ -1,11 +1,29 @@
-FROM denoland/deno:1.18.2
+FROM denoland/deno:debian
+
+RUN apt-get update && apt-get upgrade -y && apt-get install curl gnupg unzip -y
+
+# Add public key for telldus
+RUN curl http://download.telldus.com/debian/telldus-public.key | apt-key add -
+
+# Add source
+RUN echo "deb http://download.telldus.com/debian/ stable main" >> /etc/apt/sources.list
+
+# Baserat på:
+# https://forum.telldus.com/viewtopic.php?f=8&t=4&p=49723&hilit=telldus+core+ubuntu&sid=55e6f99e8ed8d235aec93253a25213e6#p49723
+
+# And install
+RUN apt-get update && \
+    apt-get install -y --no-install-recommends \
+    libftdi1 \
+    libtelldus-core2
+
+RUN apt-get download telldus-core
 
 # The port that your application listens to.
 EXPOSE 1993
 
 WORKDIR /app
 
-# Prefer not to run as root.
 USER deno
 
 # These steps will be re-run upon each file change in your working directory:
--- main.ts
+++ main.ts
@@ -1,31 +1,31 @@
-import { Client } from 'https://deno.land/x/mqtt/deno/mod.ts'; // Deno (ESM)
+import { Client } from 'https://deno.land/x/mqtt@0.1.2/deno/mod.ts'; // Deno (ESM)
 
 async function main() {
     const client = new Client({
         url: 'mqtt://mqtt.taklamakan.se',
-    });
+    })
 
-    await client.connect();
+    await client.connect()
 
-    const decoder = new TextDecoder();
+    const decoder = new TextDecoder()
 
     client.on('message', async (topic: string, payload: Uint8Array) => {
 
-        const jsondata = decoder.decode(payload);
+        const jsondata = decoder.decode(payload)
 
         switch (topic) {
             case 'tellstick':
                 const data = JSON.parse(jsondata)
                 console.log(data)
-                const p=Deno.run({ cmd: ["echo", data.cmd, data.id] });
-                await p.status();
+                const p = Deno.run({ cmd: ["echo", data.cmd, data.id] })
+                await p.status()
                 break
             default:
                 break
         }
     });
 
-    await client.subscribe('#');
+    await client.subscribe('#')
 }
 
-main();
+main()
